06be9f87837099bae0c6dfc7d5d4c5415b0ed1b5
raphaelpavani <raphaelpavani@gmail.com>
2020-03-25 03:07:57 -0300

    inserção de dados de ongs no banco de dados

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,25 @@ const express = require("express");
 
 const crypto = require("crypto");
 
+const connection = require("./database/connection");
+
 const routes = express.Router();
 
-routes.post("/ongs", (request, response) => {
-  const { name, email, whatsapp, uf } = request.body;
+routes.post("/ongs", async (request, response) => {
+  const { name, email, whatsapp, city, uf } = request.body;
 
   const id = crypto.randomBytes(4).toString("HEX");
 
-  return response.json({});
+  await connection("ongs").insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
+  });
+
+  return response.json({ id });
 });
 
 module.exports = routes;
