5e15c6ba74ddf0eb31ab0d352954f0dcc76dab60
raphaelpavani <raphaelpavani@gmail.com>
2020-03-25 20:04:48 -0300

    routes create and delete are ok

--- src/routes.js
+++ src/routes.js
@@ -1,31 +1,11 @@
 const express = require("express");
 
-const crypto = require("crypto");
-
-const connection = require("./database/connection");
+const OngControllers = require("./controllers/OngControllers");
 
 const routes = express.Router();
 
-routes.get("/ongs", async (request, response) => {
-  const ongs = await connection("ongs").select("*");
-  return response.json(ongs);
-});
+routes.get("/ongs", OngControllers.index);
 
-routes.post("/ongs", async (request, response) => {
-  const { name, email, whatsapp, city, uf } = request.body;
-
-  const id = crypto.randomBytes(4).toString("HEX");
-
-  await connection("ongs").insert({
-    id,
-    name,
-    email,
-    whatsapp,
-    city,
-    uf
-  });
-
-  return response.json({ id });
-});
+routes.post("/ongs", OngControllers.create);
 
 module.exports = routes;
