9a47219e11e5dbf808594c79575f2bd41280bbe7
raphaelpavani <raphaelpavani@gmail.com>
2020-03-25 20:51:58 -0300

    route create incidents with post method created

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 
 const OngControllers = require("./controllers/OngControllers");
+const IncidentControllers = require("./controllers/incidentControllers");
 
 const routes = express.Router();
 
 routes.get("/ongs", OngControllers.index);
 
 routes.post("/ongs", OngControllers.create);
+
+routes.post("/incidents", IncidentControllers.create);
 
 module.exports = routes;
